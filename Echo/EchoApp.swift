d41b4c003f587e9f81d04cc35fccb11498772bf6
Hallie <70453087+ha1lie@users.noreply.github.com>
2023-01-28 15:27:28 -0500

    Initial Commit

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Hallie on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
